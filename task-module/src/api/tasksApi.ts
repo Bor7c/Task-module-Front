3661e743a09a0ba2fe247d58fe24e4cdf64dcb91
Bor7c <Bob.sokolov@yandex.ru>
2025-05-11 15:49:57 +0300

    Начало новых методов задач

--- task-module/src/api/tasksApi.ts
+++ task-module/src/api/tasksApi.ts
@@ -2,14 +2,15 @@ import api from './axiosConfig';
 import { Task } from '../types/Types';
 
 export const fetchTasks = async (): Promise<Task[]> => {
+  const response = await api.get('/tasks/my-team/');
+  return response.data;
+};
+
+export const fetchAllTasks = async (): Promise<Task[]> => {
   const response = await api.get('/tasks/');
   return response.data;
 };
 
-// export const createTask = async (taskData: Omit<Task, 'id'>): Promise<Task> => {
-//   const response = await api.post('/tasks/', taskData);
-//   return response.data;
-// };
 
 export const deleteTask = async (taskId: number): Promise<void> => {
   await api.delete(`/tasks/${taskId}/`);
